1418e39b2bfd7b4fbfc7c270a3f76a8fdc880472
Bailey Kaplon <bkaplon@iu.edu>
2019-10-06 13:55:58 -0400

    Added create table statements to sqlStatements.sql

--- app/data/sqlStatements.sql
+++ app/data/sqlStatements.sql
@@ -1,1 +1,31 @@
 CREATE DATABASE ocfr_team24;
+
+CREATE TABLE Person (
+pId primary key varchar(64),
+firstName varchar(64),
+lastName varchar(64),
+Gender char(1),
+dob date,
+position varchar(64),
+radioNumber varchar(64),
+stationNumber int,
+isActive boolean,
+startDate date,
+email varchar(64),
+homePhone varchar(64),
+mobilePhone varchar(64),
+workPhone varchar(64),
+address varchar(64)
+);
+CREATE TABLE CurrentCert (
+pId foreign key varchar(64),
+cId foreign key varchar(64),
+renewedDate date,
+expDate date
+);
+CREATE TABLE Certification (
+cId primary key varchar (64),
+agency varchar(64),
+certName varchar(64),
+expiry int
+);
