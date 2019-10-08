c78390eae249f1c3dd9dcc9d738378313fbb31e5
Levi Roth <levroth@iu.edu>
2019-10-08 17:30:29 -0400

    Updated SQL Statements

--- app/data/sqlStatements.sql
+++ app/data/sqlStatements.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE ocfr_team24;
 
+UUSE ocfr_team24;
+
 CREATE TABLE Person (
-pId primary key varchar(64),
+pId varchar(64) NOT NULL PRIMARY KEY,
 firstName varchar(64),
 lastName varchar(64),
-Gender char(1),
+gender char(1),
 dob date,
 position varchar(64),
 radioNumber varchar(64),
@@ -17,15 +19,25 @@ mobilePhone varchar(64),
 workPhone varchar(64),
 address varchar(64)
 );
-CREATE TABLE CurrentCert (
-pId foreign key varchar(64),
-cId foreign key varchar(64),
-renewedDate date,
-expDate date
-);
+
 CREATE TABLE Certification (
-cId primary key varchar (64),
+cId varchar (64) NOT NULL PRIMARY KEY,
 agency varchar(64),
 certName varchar(64),
 expiry int
+);
+
+CREATE TABLE CurrentCert (
+pId varchar(64) NOT NULL,
+cId varchar(64) NOT NULL,
+renewedDate date,
+expDate date,
+PRIMARY KEY(pId, cId),
+FOREIGN KEY (pId)
+  REFERENCES Person(pId)
+  ON DELETE CASCADE,
+
+FOREIGN KEY (cId)
+  REFERENCES Certification(cId)
+  ON DELETE CASCADE
 );
