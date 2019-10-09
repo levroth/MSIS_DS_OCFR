aadf6713d415a19e7f5bca7cc5de7e0b50fd48fb
Levi Roth <levroth@iu.edu>
2019-10-09 19:24:49 -0400

    variable name changes in apis

--- app/public/api/members/index.php
+++ app/public/api/members/index.php
@@ -5,9 +5,9 @@ $db = DbConnection::getConnection();
 $stmt = $db->prepare('SELECT * FROM Person');
 $stmt->execute();
 
-$patients = $stmt->fetchAll();
+$members = $stmt->fetchAll();
 // Step 3: Convert to JSON
-$json = json_encode($patients, JSON_PRETTY_PRINT);
+$json = json_encode($members, JSON_PRETTY_PRINT);
 // Step 4: Output
 header('Content-Type: application/json');
 echo $json;
