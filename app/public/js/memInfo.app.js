c409d4e665368c65bdb86f5b584d4ff88d23b99e
Bailey Kaplon <bkaplon@iu.edu>
2019-10-15 19:35:15 -0400

    Finished formatting meminfo and certinfo. No data populating yet.

--- app/public/js/memInfo.app.js
+++ app/public/js/memInfo.app.js
@@ -1,7 +1,11 @@
 var memberRecordApp = new Vue({
   el: '#memberRecordApp',
   data: {
-      memberRecord: [],
+      memberRecord: [{
+"members":[
+  {"employeeID":"1", "firstName":"John", "lastName":"Doe", "radioNumber":"1", "stationNumber":"A-1"}
+]
+}],
       certificates: []
   },
   methods: {
