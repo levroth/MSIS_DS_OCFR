0492598f7c692acc10d41110e880ee533f681b51
ctmcgill <54513220+ctmcgill@users.noreply.github.com>
2019-10-15 19:11:33 -0400

    Created post PHP for certifications and people for when we create new

---
+++ app/public/api/certificates/post.php
@@ -0,0 +1,28 @@
+<?php
+use Ramsey\Uuid\Uuid;
+
+// Step 0: Validate the incoming data
+$guid = Uuid::uuid4()->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$stmt = $db->prepare(
+  'INSERT INTO Person (cId, agency, certName, expiry)
+  VALUES (?, ?, ?, ?,)'
+);
+
+$stmt->execute([
+  $guid,
+  $_POST['cId'],
+  $_POST['agency'],
+  $_POST['certName'],
+  $_POST['expiry'],
+
+
+]);
+
+// Step 4: Output
+header('HTTP/1.1 303 See Other');
+header('Location: ../records/?guid=' . $guid);
---
+++ app/public/api/members/post.php
@@ -0,0 +1,36 @@
+<?php
+use Ramsey\Uuid\Uuid;
+
+// Step 0: Validate the incoming data
+$guid = Uuid::uuid4()->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$stmt = $db->prepare(
+  'INSERT INTO Person (pId, firstName, lastName, gender, dob, position, radioNumber, stationNumber, isActive, startDate, email, mobilePhone, address)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+);
+
+$stmt->execute([
+  $guid,
+  $_POST['pId'],
+  $_POST['firstName'],
+  $_POST['lastName'],
+  $_POST['gender'],
+  $_POST['dob'],
+  $_POST['position'],
+  $_POST['radioNumber'],
+  $_POST['stationNumber'],
+  $_POST['isActive'],
+  $_POST['startDate'],
+  $_POST['email'],
+  $_POST['mobilePhone'],
+  $_POST['address']
+
+]);
+
+// Step 4: Output
+header('HTTP/1.1 303 See Other');
+header('Location: ../records/?guid=' . $guid);
